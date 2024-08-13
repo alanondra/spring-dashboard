8e400a23b21f3879810901beb78a7ad4d0ee8d13
Alan Ondra <r.alan.ondra@gmail.com>
2024-08-13 22:59:05 +0000

    Set up Bootstrap, Font-Awesome and Webpack manifest

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,14 @@
 const path = require('path');
+const fs = require('fs');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const RemovePlugin = require('remove-files-webpack-plugin');
-
-const envName = process.env.NODE_ENV || 'development';
-const isProduction = envName == 'production';
+const { WebpackManifestPlugin } = require('webpack-manifest-plugin');
 
 const resourcePath = './src/main/resources';
 const staticPath = path.resolve(resourcePath, 'static');
 const buildPath = path.resolve(staticPath, 'assets/build');
 const publicPrefix = '/assets/build';
+const manifestPath = path.resolve(staticPath, 'manifest.json');
 
 const resource = (res) => path.resolve(resourcePath, res);
 
@@ -21,131 +21,162 @@ const entries = {
 	}
 };
 
-module.exports = [
-	{
-		entry: entries.styles,
+let sharedManifest = {};
 
-		output: {
-			path: buildPath,
-
-			publicPath: publicPrefix,
-
-			// https://webpack.js.org/configuration/output/#outputfilename
-			filename: 'css/[name].css.js'
-		},
-
-		plugins: [
-			new MiniCssExtractPlugin({
-				filename: 'css/[name].css'
-			}),
-
-			new RemovePlugin({
-				after: {
-					test: [
-						{
-							folder: buildPath,
-							recursive: true,
-
-							method: (path) => {
-								return new RegExp(/\.css\.js$/, 'm').test(path);
-							}
-						}
-					]
-				}
-			})
-		],
-
-		module: {
-			rules: [
-				{
-					test: /\.css$/i,
-					use: [
-						MiniCssExtractPlugin.loader,
-						{
-							loader: 'css-loader',
-							options: {
-								sourceMap: false
-							}
-						}
-					]
-				},
-				{
-					test: /\.s(a|c)ss$/,
-					use: [
-						MiniCssExtractPlugin.loader,
-						{
-							loader: 'css-loader',
-							options: {
-								sourceMap: false
-							}
-						},
-						{
-							loader: 'resolve-url-loader',
-							options: {
-								keepQuery: true
-							}
-						},
-						{
-							loader: 'sass-loader',
-							options: {
-								sourceMap: true,
-								additionalData: `$env: ${envName};`
-							}
-						}
-					],
-				},
-				{
-					test: /\.(gif|jpe?g|png|svg)$/i,
-					type: 'asset/resource',
-					generator: {
-						filename: 'img/[name][ext][query]'
-					}
-				},
-				{
-					test: /\.(eot|otf|ttf|woff2?)$/i,
-					type: 'asset/resource',
-					generator: {
-						filename: 'fonts/[name][ext][query]'
-					}
-				}
-			]
-		}
-	},
-	{
-		entry: entries.scripts,
-
-		devtool: isProduction
-			? 'source-map'
-			: 'inline-source-map',
-
-		plugins: [
-			//
-		],
-
-		output: {
-			path: buildPath,
-
-			publicPath: publicPrefix,
-
-			filename: 'js/[name].js'
-		},
-
-		module: {
-			rules: [
-				{
-					test: /\.tsx?$/,
-					use: 'ts-loader',
-					exclude: /node_modules/
-				}
-			]
-		},
-
-		resolve: {
-			extensions: [
-				'.tsx',
-				'.ts',
-				'.js'
-			]
-		}
+const clearManifest = () => {
+	if (fs.existsSync(manifestPath)) {
+		fs.unlinkSync(manifestPath);
 	}
-];
+};
+
+module.exports = (env, argv) => {
+	clearManifest();
+
+	const mode = argv.mode || 'development';
+	const isProduction = mode === 'production';
+
+	return [
+		{
+			entry: entries.styles,
+
+			output: {
+				path: buildPath,
+				publicPath: publicPrefix,
+				// https://webpack.js.org/configuration/output/#outputfilename
+				filename: 'css/[name].css.js'
+			},
+
+			plugins: [
+				new MiniCssExtractPlugin({
+					// https://webpack.js.org/configuration/output/#outputfilename
+					filename: isProduction
+						? 'css/[name].[contenthash:8].css'
+						: 'css/[name].css'
+				}),
+
+				new RemovePlugin({
+					after: {
+						test: [
+							{
+								folder: buildPath,
+								recursive: true,
+								method: (filePath) => /\.css\.js$/.test(filePath)
+							}
+						]
+					}
+				}),
+
+				new WebpackManifestPlugin({
+					fileName: path.relative(buildPath, manifestPath),
+					publicPath: publicPrefix,
+					seed: sharedManifest,
+					filter: (file) => /\.css$/.test(file.name)
+				})
+			],
+
+			module: {
+				rules: [
+					{
+						test: /\.css$/i,
+						use: [
+							MiniCssExtractPlugin.loader,
+							{
+								loader: 'css-loader',
+								options: {
+									sourceMap: true
+								}
+							}
+						]
+					},
+					{
+						test: /\.s(a|c)ss$/,
+						use: [
+							MiniCssExtractPlugin.loader,
+							{
+								loader: 'css-loader',
+								options: {
+									sourceMap: true
+								}
+							},
+							{
+								loader: 'resolve-url-loader',
+								options: {
+									keepQuery: true,
+									sourceMap: true,
+									root: staticPath
+								}
+							},
+							{
+								loader: 'sass-loader',
+								options: {
+									api: 'legacy',
+									implementation: require('sass'),
+									sourceMap: true,
+									additionalData: `$env: ${mode};`,
+									sassOptions: {
+										style: 'compressed',
+										quietDeps: true,
+										sourceMap: true
+									}
+								}
+							}
+						]
+					},
+					{
+						test: /\.(gif|jpe?g|png|svg)$/i,
+						type: 'asset/resource',
+						generator: {
+							filename: 'img/[name][ext][query]'
+						}
+					},
+					{
+						test: /\.(eot|otf|ttf|woff2?)$/i,
+						type: 'asset/resource',
+						generator: {
+							filename: 'fonts/[name][ext][query]'
+						}
+					}
+				]
+			}
+		},
+		{
+			entry: entries.scripts,
+
+			devtool: isProduction
+				? 'source-map'
+				: 'inline-source-map',
+
+			output: {
+				path: buildPath,
+				publicPath: publicPrefix,
+				// https://webpack.js.org/configuration/output/#outputfilename
+				filename: isProduction
+					? 'js/[name].[contenthash:8].js'
+					: 'js/[name].js'
+			},
+
+			plugins: [
+				new WebpackManifestPlugin({
+					fileName: path.relative(buildPath, manifestPath),
+					publicPath: publicPrefix,
+					seed: sharedManifest,
+					filter: (file) => /\.js$/.test(file.name)
+				})
+			],
+
+			module: {
+				rules: [
+					{
+						test: /\.tsx?$/,
+						use: 'ts-loader',
+						exclude: /node_modules/
+					}
+				]
+			},
+
+			resolve: {
+				extensions: ['.tsx', '.ts', '.js']
+			}
+		}
+	];
+};
