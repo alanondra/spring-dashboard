af3264a0853ae55a87bfdec946675d070b9e002c
Alan Ondra <r.alan.ondra@gmail.com>
2024-08-13 14:00:42 +0000

    Replace Thymeleaf with Pebble

--- src/main/java/com/aondra/dashboard/http/controllers/DefaultController.java
+++ src/main/java/com/aondra/dashboard/http/controllers/DefaultController.java
@@ -2,13 +2,14 @@ package com.aondra.dashboard.http.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class DefaultController extends AbstractController
 {
 	@GetMapping("/")
-	public String index()
+	public ModelAndView index()
 	{
-		return "hello, world";
+		return new ModelAndView("pages/index");
 	}
 }
